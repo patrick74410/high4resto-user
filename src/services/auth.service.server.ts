e9ff335877447012093460b603876208a22c39c2
PapyElGringo <adrienpeslerbe@gmail.com>
2020-12-03 17:23:11 +0100

    fix universal polyfill

--- src/services/auth.service.server.ts
+++ src/services/auth.service.server.ts
@@ -2,11 +2,12 @@
  * This service is a SHIM for Angular Universal Server Side for Auth0
  */
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
     providedIn: 'root',
 })
 export class ServerAuthService {
     isAuthenticated$ = new BehaviorSubject(false);
+    user$ = new Observable();
 }
